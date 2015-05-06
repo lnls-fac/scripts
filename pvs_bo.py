e8dd30aa92a8218f6ac51c2d999ccac2af577ffd
ximenes <xresende@gmail.com>
2015-05-06 10:20:30 -0300

    Update pvs_bo.py
    - so that it returns PVs with added prefix

---
+++ pvs_bo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import sirius
+
+"""Return one-line string with Sirius SI proccess variables"""
+
+try:
+    prefix = sys.argv[1]
+except:
+    prefix = ''
+
+record_names = sirius.bo.record_names.get_record_names()
+pvs_list = list(record_names.keys())
+pvs_string = prefix + (' '+prefix).join(pvs_list)
+print(pvs_string, end='')
