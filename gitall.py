a775c3094e7c7ca311d7ef2be30a8f2ab074ff54
ximenes <xresende@gmail.com>
2015-05-09 02:07:17 -0300

    Change set of git_*all routines
    - all these routines have been replaced with 'gitall.py' python script that takes
      a argument which specified what git command (status,pull,push,diff) to run for
      each repo. it can also take 'clone'.

---
+++ gitall.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import lnls
+#import git
+#from termcolor import colored
+import subprocess
+
+git_functions = ('pull','push','status','diff','clone')
+
+
+
+def run_git_clone():
+
+    if not os.path.exists(lnls.folder_code):
+        print('gitall.py: please create ' + lnls.folder_code + ' folder with correct permissions first!')
+        return
+
+    all_repos = ('collective_effects',
+                 'fieldmaptrack',
+                 'job_manager',
+                 'lnls',
+                 'mathphys',
+                 'MatlabMiddleLayer',
+                 'pyaccel',
+                 'scripts',
+                 'sirius',
+                 'sirius_wiki',
+                 'tools',
+                 'trackcpp',
+                 'tracy_sirius',
+                 'va',
+                 )
+
+    for repo in all_repos:
+        cmd = 'git clone ssh://git@github.com/lnls-fac/' + repo + '.git'
+        os.system(cmd)
+
+def run_git(func):
+
+    if func == 'clone': return run_git_clone()
+
+    fnames = os.listdir(lnls.folder_code)
+    for fname in fnames:
+        repo_folder = os.path.join(lnls.folder_code, fname)
+        print('processing ' + func + ' <'+fname+'>...')
+        cmd = 'cd ' + repo_folder + '; git ' + func
+        text = subprocess.call([cmd], shell=True, stdout=sys.stdout)
+        print('...ok')
+        print()
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2 or sys.argv[1] not in git_functions:
+        print('usage: gitall.py [' + '|'.join(git_functions) + ']')
+    else:
+        print()
+        run_git(sys.argv[1])
