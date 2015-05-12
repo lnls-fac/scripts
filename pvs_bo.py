27d68b9c6a6e27439eef15d1fdc9498245915cc3
ximenes <xresende@gmail.com>
2015-05-12 17:44:48 -0300

    Change in pvs_bo and pvs_si

--- pvs_bo.py
+++ pvs_bo.py
@@ -2,6 +2,7 @@
 
 import sys
 import sirius
+import va
 
 """Return one-line string with Sirius SI proccess variables"""
 
@@ -10,7 +11,8 @@ try:
 except:
     prefix = ''
 
+fake_names = va.bo_fake_record_names.get_record_names()
 record_names = sirius.bo.record_names.get_record_names()
-pvs_list = list(record_names.keys())
+pvs_list = list(record_names.keys()) + list(fake_names.keys())
 pvs_string = prefix + (' '+prefix).join(pvs_list)
 print(pvs_string, end='')
--- pvs_si.py
+++ pvs_si.py
@@ -2,6 +2,7 @@
 
 import sys
 import sirius
+import va
 
 """Return one-line string with Sirius SI proccess variables"""
 
@@ -10,9 +11,8 @@ try:
 except:
     prefix = ''
 
+fake_names = va.si_fake_record_names.get_record_names()
 record_names = sirius.si.record_names.get_record_names()
-pvs_list = list(record_names.keys())
+pvs_list = list(record_names.keys()) + list(fake_names.keys())
 pvs_string = prefix + (' '+prefix).join(pvs_list)
 print(pvs_string, end='')
-
-
