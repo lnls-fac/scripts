7e23b9cd9108b7473eb31566194cad89f10c8d1b
ximenes <xresende@gmail.com>
2015-05-29 10:04:51 -0300

    Add ts and tb to sirius-pvs-prefix.sh

--- bin/sirius-pvs-prefix.sh
+++ bin/sirius-pvs-prefix.sh
@@ -2,7 +2,7 @@
 # completions for epics commands are implemented.
 
 PREFIX=$1
-ALLPVS="`sirius-pvs-li.py $PREFIX` `sirius-pvs-bo.py $PREFIX` `sirius-pvs-si.py $PREFIX` `sirius-pvs-ti.py $PREFIX`"
+ALLPVS="`sirius-pvs-li.py $PREFIX` `sirius-pvs-tb.py $PREFIX` `sirius-pvs-bo.py $PREFIX` `sirius-pvs-ts.py $PREFIX` `sirius-pvs-si.py $PREFIX` `sirius-pvs-ti.py $PREFIX`"
 complete -W "$ALLPVS" caget
 complete -W "$ALLPVS" camonitor
 complete -W "$ALLPVS" caput
