4b5eea42125a650f8024911d6043e0c9fbe6e492
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-07-15 10:29:22 -0300

    Generalise fac-cloneall-sh

--- bin/fac-cloneall.sh
+++ bin/fac-cloneall.sh
@@ -9,19 +9,28 @@ else
 fi
 
 echo "Cloning repositories..."
-git clone ssh://git@github.com/lnls-fac/collective_effects.git
-git clone ssh://git@github.com/lnls-fac/fieldmaptrack.git
-git clone ssh://git@github.com/lnls-fac/job_manager.git
-git clone ssh://git@github.com/lnls-fac/lnls.git
-git clone ssh://git@github.com/lnls-fac/mathphys.git
-git clone ssh://git@github.com/lnls-fac/MatlabMiddleLayer.git
-git clone ssh://git@github.com/lnls-fac/pyaccel.git
-git clone ssh://git@github.com/lnls-fac/sirius.git
-git clone ssh://git@github.com/lnls-fac/sirius_wiki.git
-git clone ssh://git@github.com/lnls-fac/tools.git
-git clone ssh://git@github.com/lnls-fac/trackcpp.git
-git clone ssh://git@github.com/lnls-fac/tracy_sirius.git
-git clone ssh://git@github.com/lnls-fac/va.git
+
+for REPOSITORY in \
+    collective_effects \
+    fieldmaptrack \
+    job_manager \
+    lnls \
+    mathphys \
+    MatlabMiddleLayer \
+    pyaccel \
+    scripts \
+    sirius \
+    sirius_wiki \
+    tools \
+    trackcpp \
+    tracy_sirius \
+    va
+do
+    if [ ! -d "$REPOSITORY" ] ; then
+        git clone ssh://git@github.com/lnls-fac/"$REPOSITORY".git
+    fi
+done
+
 echo "Finished cloning repositories."
 
 if [ ! -z $FACCODE ] ; then
