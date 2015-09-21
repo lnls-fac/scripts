4646fc413cffe795736ebf882ceaf9d8e6226cc9
Fernando Henrique de Sá <fernando@lnls210-linux.abtlus.org.br>
2015-09-21 17:46:14 -0300

    Creation of script which gets status and IP of all available hosts for pyjob in the same subnet as the computer where it was called.

---
+++ bin/get_hosts_subnet.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import nmap
+import socket
+from uuid import getnode as get_mac
+
+STRFORMAT = '{0:18s} {1:6s} {2:15s}'
+AVAILABLE_HOSTS = dict(
+    {'10.0.7':
+        {'08:00:27:F8:38:3A':'lnls340-virtual',
+         '08:00:27:86:C6:0F':'lnls343-1-virtual',
+         '08:00:27:34:7C:12':'lnls343-2-virtual',
+         '08:00:27:35:25:D6':'lnls138-virtual',
+         '08:00:27:10:29:80':'lnls119-virtual',
+         '08:00:27:84:59:65':'lnls116-virtual',
+         '08:00:27:CB:3D:D7':'lnls350-1-virtual',
+         '08:00:27:13:93:5D':'lnls350-2-virtual',
+         '08:00:27:3F:9E:DB':'lnls346-virtual'},
+     '10.0.21':
+        {'AC:16:2D:34:C8:A5':'fernando-linux',
+         'AC:16:2D:34:C8:F3':'ximenes-linux',
+         'AC:16:2D:34:BD:C5':'luana-linux',
+         '00:1E:C9:21:9C:06':'afonso-linux',
+         '08:00:27:95:0D:DE':'lnls155-virtual'},
+     '10.0.28':
+        {'AC:16:2D:34:6A:43':'liu-linux',
+         '3c:d9:2b:70:d2:d8':'lnls82-linux',
+         '1c:c1:de:66:1b:55':'lnls54-linux'}     
+         })
+
+my_mac = str(hex(get_mac()))
+my_mac = ':'.join([my_mac[i:i+2].upper() for i in range(2,len(my_mac),2)])
+my_ip  = socket.gethostbyname_ex(socket.gethostname())[2][1]
+my_subnet = '.'.join(my_ip.split('.')[0:3])
+
+hosts_in_my_subnet = AVAILABLE_HOSTS[my_subnet]
+
+nm = nmap.PortScanner()
+scan = nm.scan(hosts='10.0.21.0/24',arguments='-sP',sudo=True)
+result = scan['scan']
+
+hosts_online = {result[x]['addresses']['mac']:x 
+                for x in result.keys() if not x.startswith('10.0.21.98')}
+
+
+print(STRFORMAT.format('Hosts', 'State', 'IP'))
+for mac, name in hosts_in_my_subnet.items():
+    if mac in hosts_online.keys():
+        print(STRFORMAT.format(name,'up', hosts_online[mac]))
+    else:
+        if not mac.startswith(my_mac):
+            print(STRFORMAT.format(name,'down',''))
+        else:
+            print(STRFORMAT.format(name,'up', my_ip))
+
+
