c3fd401c31c80777ec793d103d43a2d0b86d924a
Fernando <fernandohds564@gmail.com>
2016-06-23 17:35:54 -0300

    test commit 2.

--- bin/fac-data-gitall.py
+++ bin/fac-data-gitall.py
@@ -12,6 +12,10 @@ for repo in repos:
     rep  = repo.decode().rpartition('/')[0]
     sh.cd(rep)
     print(rep.rpartition('/')[-1])
-    status_repo = sh.git.status()
-    print(status_repo)
+    status_repo = sh.git.status().stdout.decode()
+    if status_repo.find('nothing to commit, working directory clean') < 0:
+        add_repo    = sh.git.add('*')
+        commit_repo = sh.git.commit(m=msg_commit)
+        pull_repo   = sh.git.pull()
+
     print(sh.ls(sh.HOME))
