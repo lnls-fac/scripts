ac373432552ee60068662f340e2bd24891a44ef3
Fernando <fernandohds564@gmail.com>
2016-06-23 19:12:07 -0300

    testing

--- bin/fac-data-gitall.py
+++ bin/fac-data-gitall.py
@@ -12,10 +12,12 @@ for repo in repos:
     rep  = repo.decode().rpartition('/')[0]
     sh.cd(rep)
     print(rep.rpartition('/')[-1])
-    status_repo = sh.git.status().stdout.decode()
-    if status_repo.find('nothing to commit, working directory clean') < 0:
+    status_repo = sh.git.status(porcelain=True).stdout.decode()
+    if status_repo:
         add_repo    = sh.git.add('*')
-        commit_repo = sh.git.commit(m=msg_commit)
-        pull_repo   = sh.git.pull()
+        commit_repo = sh.git.commit(a=True,m=msg_commit)
+
+
+    pull_repo   = sh.git.pull()
 
     print(sh.ls(sh.HOME))
