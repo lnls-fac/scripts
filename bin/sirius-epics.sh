a06619caaa1f265fc588dbbebae0541aeef14a99
ximenes <xresende@gmail.com>
2017-01-18 13:48:45 -0200

    Update in sirius-vaca-checkout.sh

--- bin/sirius-epics.sh
+++ bin/sirius-epics.sh
@@ -3,12 +3,18 @@
 
 
 if [ "$0" != "bash" ]; then
-    echo "this script should be sourced insteaf of executed!"
+    echo "this script should be sourced instead of executed!"
     exit
 fi
 
 IP=$1
-if [ -z $IP ]; then IP="10.0.7.78"; fi
+if [ -z $IP ]; then
+    # descubro o ip da máquina
+    IP=$( ifconfig | grep -A 2 eth0 | grep "inet add" | cut --delimiter=":" -f 2 | cut --delimiter="B" -f 1 )
+    if [ -z "$IP" ]; then
+        IP=$( ifconfig | grep -A 2 eth1 | grep "inet add" | cut --delimiter=":" -f 2 | cut --delimiter="B" -f 1 )
+    fi
+fi
 
 export EPICS_CA_ADDR_LIST=$IP
 export EPICS_CA_AUTO_ADDR_LIST=no
--- bin/sirius-vaca-checkout.sh
+++ bin/sirius-vaca-checkout.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-function checkout_commits {
+function checkout_release_branches {
 
   cd $FACCODE/va; ans1=$(git status | grep "nothing to commit");
   cd $FACCODE/sirius; ans2=$(git status | grep "nothing to commit");
@@ -10,35 +10,49 @@ function checkout_commits {
   if [ -z "$ans1" -o -z "$ans2" -o -z "$ans3" ]; then
     printf "At least one of the repositories has modifications. bailing out...\n"
   else
-    printf "checking va: "; cd $FACCODE/va; git checkout 127d000; sudo python3 ./setup.py develop
-    printf "checking sirius: "; cd $FACCODE/sirius; git checkout 6bbfffe; sudo python3 ./setup.py develop
-    printf "checking siriusdb: "; cd $FACROOT/siriusdb; git checkout d76088f
+    printf "<va>\n"; cd $FACCODE/va; git checkout release-v0.15.1; sudo python3 ./setup.py develop; printf "\n"
+    printf "<sirius>\n"; cd $FACCODE/sirius; git checkout release-v0.11.2; sudo python3 ./setup.py develop; printf "\n"
+    printf "<siriusdb>\n"; cd $FACROOT/siriusdb; git checkout release-v0.0.1; printf "\n"
   fi
 
 }
 
-function checkout_masters {
+function checkout_master_branches {
 
-  cd $FACCODE/va; ans1=$(git status | grep "nothing to commit");
-  cd $FACCODE/sirius; ans2=$(git status | grep "nothing to commit");
-  cd $FACROOT/siriusdb; ans3=$(git status | grep "nothing to commit");
-
-  if [ -z "$ans1" -o -z "$ans2" -o -z "$ans3" ]; then
-    printf "At least one of the repositories has modifications. bailing out...\n"
-  else
-    printf "checking va: "; cd $FACCODE/va; git checkout master; sudo python3 ./setup.py develop
-    printf "checking sirius: "; cd $FACCODE/sirius; git checkout master; sudo python3 ./setup.py develop
-    printf "checking siriusdb: "; cd $FACROOT/siriusdb; git checkout master
-  fi
+  printf "<va>\n"; cd $FACCODE/va; git checkout master; sudo python3 ./setup.py develop; printf "\n"
+  printf "<sirius>\n"; cd $FACCODE/sirius; git checkout master; sudo python3 ./setup.py develop; printf "\n"
+  printf "<siriusdb>\n"; cd $FACROOT/siriusdb; git checkout master; printf "\n"
 
 }
+
+function set_envs {
+    # descubro o ip da máquina
+    IP=$( ifconfig | grep -A 2 eth0 | grep "inet add" | cut --delimiter=":" -f 2 | cut --delimiter="B" -f 1 )
+    if [ -z "$IP" ]; then
+        IP=$( ifconfig | grep -A 2 eth1 | grep "inet add" | cut --delimiter=":" -f 2 | cut --delimiter="B" -f 1 )
+    fi
+    export EPICS_CA_ADDR_LIST=$IP
+    export EPICS_CA_AUTO_ADDR_LIST=no
+}
+
+
+function checkout_release_run {
+
+    checkout_release_branches
+    set_envs
+    sirius-vaca.py
+
+}
+
+
+  
 
 if [ "$#" -eq 0 ]; then
-  checkout_commits
-elif [ "$#" -eq 1 -a "$1" == "commits" ]; then
-  checkout_commits
+  checkout_release_run
+elif [ "$#" -eq 1 -a "$1" == "releases" ]; then
+  checkout_release_branches
 elif [ "$#" -eq 1 -a "$1" == "masters" ]; then
-  checkout_masters
+  checkout_master_branches
 else
   printf "Invalid number of arguments!\n"
 fi
