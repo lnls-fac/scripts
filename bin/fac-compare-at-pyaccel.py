872df4cf82cb7c8b6d250d7cccda85e872e044c5
ximenes <xresende@gmail.com>
2018-01-03 14:42:32 -0200

    Rename sirius-test-lattice.py

--- bin/fac-compare-at-pyaccel.py
+++ bin/fac-compare-at-pyaccel.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+"""Test lattice models.
+
+This script is used to generate tracking data from pyaccel lattice models
+in ordre for AT comparisons.
+
+(see Matlab 'lnls_compare_at_pyaccel.m')
+
+"""
+
 import pymodels
 import pyaccel
 import sys
@@ -12,7 +21,7 @@ def run_test_si():
     girders = pyaccel.lattice.find_indices(latt, attribute_name='fam_name', value='girder')
     non_girders = list(set(range(len(latt))) - set(girders))
     latt = latt[non_girders]
-    r = pyaccel.tracking.linepass(latt, p0, indices='open')
+    r = pyaccel.tracking.line_pass(latt, p0, indices='open')
     traj = r[0]
     for i in range(traj.shape[1]):
         t = traj[:,i]; print('{0:+.17e} {1:+.17e} {2:+.17e} {3:+.17e} {4:+.17e} {5:+.17e}'.format(t[0],t[1],t[2],t[3],t[4],t[5]))
@@ -25,7 +34,7 @@ def run_test_ts():
     girders = pyaccel.lattice.find_indices(latt, attribute_name='fam_name', value='girder')
     non_girders = list(set(range(len(latt))) - set(girders))
     latt = latt[non_girders]
-    r = pyaccel.tracking.linepass(latt, p0, indices='open')
+    r = pyaccel.tracking.line_pass(latt, p0, indices='open')
     traj = r[0]
     for i in range(traj.shape[1]):
         t = traj[:,i]; print('{0:+.17e} {1:+.17e} {2:+.17e} {3:+.17e} {4:+.17e} {5:+.17e}'.format(t[0],t[1],t[2],t[3],t[4],t[5]))
@@ -38,7 +47,7 @@ def run_test_bo():
     girders = pyaccel.lattice.find_indices(latt, attribute_name='fam_name', value='girder')
     non_girders = list(set(range(len(latt))) - set(girders))
     latt = latt[non_girders]
-    r = pyaccel.tracking.linepass(latt, p0, indices='open')
+    r = pyaccel.tracking.line_pass(latt, p0, indices='open')
     traj = r[0]
     for i in range(traj.shape[1]):
         t = traj[:,i]; print('{0:+.17e} {1:+.17e} {2:+.17e} {3:+.17e} {4:+.17e} {5:+.17e}'.format(t[0],t[1],t[2],t[3],t[4],t[5]))
@@ -51,7 +60,7 @@ def run_test_tb():
     girders = pyaccel.lattice.find_indices(latt, attribute_name='fam_name', value='girder')
     non_girders = list(set(range(len(latt))) - set(girders))
     latt = latt[non_girders]
-    r = pyaccel.tracking.linepass(latt, p0, indices='open')
+    r = pyaccel.tracking.line_pass(latt, p0, indices='open')
     traj = r[0]
     for i in range(traj.shape[1]):
         t = traj[:,i]; print('{0:+.17e} {1:+.17e} {2:+.17e} {3:+.17e} {4:+.17e} {5:+.17e}'.format(t[0],t[1],t[2],t[3],t[4],t[5]))
