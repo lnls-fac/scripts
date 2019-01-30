2dff14cce08e5603da264581a23515ca4258fb41
Guilherme <guilherme.prado@lnls.br>
2019-01-30 10:01:01 -0200

    Run scripts with sudo

--- workstation_setup/setup_workspace.sh
+++ workstation_setup/setup_workspace.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # User interaction
-./1_create_users.sh
+sudo ./1_create_users.sh
 
 # Optional args
-./2_install_git.sh
+sudo ./2_install_git.sh
 
-./3_fac_directories.sh
-./4_install_bashrc.sh
-./5_install_python.sh
-./6_install_epics.sh
-./7_install_python_deps.sh
+sudo ./3_fac_directories.sh
+sudo ./4_install_bashrc.sh
+sudo ./5_install_python.sh
+sudo ./6_install_epics.sh
+sudo -HE ./7_install_python_deps.sh
 # User interaction
-./8_intall_qt.sh
+sudo ./8_intall_qt.sh
 
-./9_install_sip.sh
-./10_install_pyqt.sh
-./11_install_fac_deps.sh
+sudo ./9_install_sip.sh
+sudo ./10_install_pyqt.sh
+sudo ./11_install_fac_deps.sh
 
 # ./12_install_repository.sh
