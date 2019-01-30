f54c7c399d8268ed2e0d32bd98a89801438577ae
Guilherme <guilherme.prado@lnls.br>
2019-01-30 09:57:56 -0200

    Removed pyepics from install_fac_deps

--- workstation_setup/11_install_fac_deps.sh
+++ workstation_setup/11_install_fac_deps.sh
@@ -2,6 +2,6 @@
 
 apt-get install libffi6 libffi-dev libfreetype6 \
 	libfreetype6-dev libpng3 nmap dvipng
-pip3 install python-nmap wakeonlan pyepics requests pyqtgraph pandas \
+pip3 install python-nmap wakeonlan requests pyqtgraph pandas \
 	psutil termcolor sh cairocffi matplotlib scipy jupyter
 
