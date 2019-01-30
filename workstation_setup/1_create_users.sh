f1b6ad611000d57542fb7ee541ecbaf50f5e1081
Fernando <fernandohds564@gmail.com>
2019-01-30 15:16:04 -0200

    ENH: Check if users exist before creating them.

--- workstation_setup/1_create_users.sh
+++ workstation_setup/1_create_users.sh
@@ -3,19 +3,27 @@
 # Create users.
 
 # Needs user interaction
+function user_exists {
+	id -u $1 >/dev/null 2>&1
+	echo $?
+}
 
-user_exists=$(id -u 'fac' >/dev/null 2>&1; echo $?)
-
-if [ $user_exists -eq 0 ]; then
+if [ $(user_exists fac) -eq 0 ]; then
 	echo 'fac user already exists'
 else
+	# echo 'fac user does not exist'
 	adduser --system fac
 	usermod -aG sudo fac
 fi
 
 users=(fernando ximenes guilherme liulin ana alexandre murilo)
 
-for user in $users; do
-	adduser $user --ingroup fac
-	usermod -aG sudo $user
+for user in ${users[@]}; do
+	if [ $(user_exists $user) -eq 0 ]; then
+		echo $user' user already exists'
+	else
+		# echo $user' does not exist'
+		adduser $user --ingroup fac
+		usermod -aG sudo $user
+	fi
 done
