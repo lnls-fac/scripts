018000258d98915325ccee2e68e14a58d78eed94
Guilherme <guilherme.prado@lnls.br>
2019-02-04 14:29:23 -0200

    Adapted create user scripts to be promptless

--- workstation_setup/1_create_users.sh
+++ workstation_setup/1_create_users.sh
@@ -7,23 +7,40 @@ function user_exists {
 	id -u $1 >/dev/null 2>&1
 	echo $?
 }
+# # Add user/group fac
+# if [ $(user_exists fac) -eq 0 ]; then
+# 	echo 'fac user already exists'
+# else
+# 	adduser fac --gecos
+# 	usermod -aG sudo fac
+# fi
+# # Add user/group sirius
+# if [ $(user_exists fac) -eq 0 ]; then
+# 	echo 'sirius user already exists'
+# else
+# 	adduser sirius --gecos '' --disabled-password
+# 	usermod -aG sudo sirius
+# 	echo "sirius:boo500mev" | sudo chpasswd
+# fi
 
-if [ $(user_exists fac) -eq 0 ]; then
-	echo 'fac user already exists'
-else
-	# echo 'fac user does not exist'
-	adduser --system fac
-	usermod -aG sudo fac
-fi
+groups=(sirius fac)
+for user in ${groups[@]}; do
+	if [ $(user_exists $user) -eq 0 ]; then
+		echo $user' user already exists'
+	else
+		adduser $user --gecos '' --disabled-password
+		usermod -aG sudo $user
+		echo "$user:boo500mev" | sudo chpasswd
+	fi
+done
 
 users=(fernando ximenes guilherme liulin ana alexandre murilo)
-
 for user in ${users[@]}; do
 	if [ $(user_exists $user) -eq 0 ]; then
 		echo $user' user already exists'
 	else
-		# echo $user' does not exist'
-		adduser $user --ingroup fac
+		adduser $user --ingroup fac --gecos '' --disabled-password
 		usermod -aG sudo $user
+		echo "$user:boo500mev" | sudo chpasswd
 	fi
 done
