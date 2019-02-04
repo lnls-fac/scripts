f2de35c23422069f374043626286fdf30b0d65e9
Guilherme <guilherme.prado@lnls.br>
2019-02-04 15:16:58 -0200

    Remove prompy from git

--- workstation_setup/2_install_git.sh
+++ workstation_setup/2_install_git.sh
@@ -3,18 +3,11 @@
 # Check if git is already installed
 ! command -v git >/dev/null 2>&1 || { echo >&2 "Git already installed. Aborting."; exit 1; }
 
-apt-get install git
+apt-get install -y git
 
 if [ $# -ne 2 ]; then
 	git config --global core.editor vim
 	git config --global push.default simple
-
-	read -p 'Email: ' email
-	read -p 'Name: ' name
-
-	git config --global user.email "$email"
-	git config --global user.name "$name"
-
 else
 	git config --global core.editor vim
 	git config --global push.default simple
