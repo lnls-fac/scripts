615f47cc7961bc5b1b5568ace1ccc978f8b2fe34
Guilherme <guilherme.prado@lnls.br>
2019-02-05 13:15:21 -0200

    Add EPICS env vars necessary to install pcaspy

--- workstation_setup/7_install_python_deps.sh
+++ workstation_setup/7_install_python_deps.sh
@@ -5,6 +5,10 @@
 # Run with sudo -HE
 
 sudo apt-get install -y python3-pip swig
+
+export EPICS_BASE=/opt/epics/base
+export EPICS_HOST_ARCH=linux-x86_64
+
 sudo -HE pip3 install pyepics==3.3.3
 sudo -HE pip3 install pcaspy==0.7.2
 exit 0
