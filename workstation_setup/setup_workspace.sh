0308bd7920d315ba9d80811b961cb5350963a0ca
Guilherme <guilherme.prado@lnls.br>
2019-02-08 17:13:25 -0200

    Prompt user to copy or create a ssh key

--- workstation_setup/setup_workspace.sh
+++ workstation_setup/setup_workspace.sh
@@ -30,8 +30,26 @@ execute 9_install_sip.sh
 execute 10_install_pyqt.sh
 execute 11_install_fac_deps.sh
 
+# Check ssh key and prompt user for action
 if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
-	echo 'In order to continue please log as a user with a ssh key configured in GitHub. Aborting.'
+	echo 'In order to continue you need an ssh-key configured to yout GitHub account.'
+	read -p "Copy from existing host[y/N]:" answer
+	if [ $answer == 'y' ] ; then
+		echo '\n\nEnter host information'
+		read -p "User:" user
+		read -p "Host:" host
+		scp -r $user@$host:/home/$user/.ssh /home/$(whoami)/
+	else
+		read -p "Create one[y/N]:" answer
+		if [ $answer == 'y' ] ; then
+			ssh-keygen
+			./set_ssh_key_github.sh
+		fi
+	fi
+fi
+# Check ssh key
+if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+	echo 'No SSH key found. Aborting.'
 	exit 1
 fi
 
@@ -47,3 +65,4 @@ execute '12_install_repository.sh' 'machine-applications'
 execute '12_install_repository.sh' 'pruserial485'
 execute '12_install_repository.sh' 'sirius-scripts'
 execute '12_install_repository.sh' 'pyjob'
+
