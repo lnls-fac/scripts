5b8a22ff27c3678f197285272cd9defbde125168
Guilherme <guilherme.prado@lnls.br>
2019-02-08 16:08:18 -0200

    Add script to set ssh key on GitHub

---
+++ workstation_setup/set_ssh_key_github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Username:" username
+read -s -p "Password:" password
+
+ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
+title="$(whoami) on $(hostname)"
+
+curl -u "$username:$password" --data "{\"title\": \"$title\",\"key\":\"$ssh_key\"}" https://api.github.com/user/keys
+
