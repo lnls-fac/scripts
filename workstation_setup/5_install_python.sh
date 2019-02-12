de3463cf93f3d6ee0336212857d5ce4e413b7c9d
Guilherme <guilherme.prado@lnls.br>
2019-02-12 10:00:38 -0200

    Install python deps after check if python is already installed

--- workstation_setup/5_install_python.sh
+++ workstation_setup/5_install_python.sh
@@ -3,22 +3,26 @@
 set -e
 set -x
 
-sudo apt-get install -y build-essential 
-sudo apt-get install -y checkinstall
-sudo apt-get install -y libreadline-gplv2-dev
-sudo apt-get install -y libncursesw5-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libsqlite3-dev 
-sudo apt-get install -y tk-dev
-sudo apt-get install -y libgdbm-dev
-sudo apt-get install -y libc6-dev
-sudo apt-get install -y libbz2-dev 
+function install_python_deps {
+	sudo apt-get install -y build-essential 
+	sudo apt-get install -y checkinstall
+	sudo apt-get install -y libreadline-gplv2-dev
+	sudo apt-get install -y libncursesw5-dev
+	sudo apt-get install -y libssl-dev
+	sudo apt-get install -y libsqlite3-dev 
+	sudo apt-get install -y tk-dev
+	sudo apt-get install -y libgdbm-dev
+	sudo apt-get install -y libc6-dev
+	sudo apt-get install -y libbz2-dev 
+}
 
 
 if [[ "$(python-sirius --version)" =~ ^Python\ 3\.6\.[0-9]$ ]]; then
 	echo "Python already installed. Passing."
 	exit 0
 fi
+
+install_python_deps
 
 if [[ "$(python3 --version)" =~ ^Python\ 3\.6\.[0-9]$ ]]; then
 	sudo ln -sf /usr/bin/pip3 /usr/local/bin/pip-sirius
