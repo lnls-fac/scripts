85d266a8a2505b7babbc75d2ac0ce7d3ea16942f
ximenes <xresende@gmail.com>
2019-03-26 21:05:51 -0300

    Delete or comment out deprecated scripts

--- bin/sirius-recordnames-prefix.sh
+++
@@ -1,9 +0,0 @@
-# This file should be sources with a prefix argument so that
-# completions for epics commands are implemented.
-
-PREFIX=$1
-ALLPVS="`sirius-pvs.py li $PREFIX` `sirius-pvs.py tb $PREFIX` `sirius-pvs.py bo $PREFIX` `sirius-pvs.py ts $PREFIX` `sirius-pvs.py si $PREFIX`"
-complete -W "$ALLPVS" caget
-complete -W "$ALLPVS" camonitor
-complete -W "$ALLPVS" caput
-complete -W "$ALLPVS" cainfo
